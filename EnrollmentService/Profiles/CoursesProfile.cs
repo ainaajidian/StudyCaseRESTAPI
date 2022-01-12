19c3ae03ab24c33b12061049c7e232836416e557
ainaajidian <ainaajidian@gmail.com>
2022-01-12 10:27:51 +0700

    Add Profile

---
+++ EnrollmentService/Profiles/CoursesProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EnrollmentService.Dtos;
+using EnrollmentService.Models;
+
+namespace EnrollmentService.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<CourseCreateDto, Course>();
+            CreateMap<CourseDto, Course>();
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
---
+++ EnrollmentService/Profiles/EnrollmentProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EnrollmentService.Dtos;
+using EnrollmentService.Models;
+
+namespace EnrollmentService.Profiles
+{
+    public class EnrollmentProfile : Profile
+    {
+        public EnrollmentProfile()
+        {
+            CreateMap<EnrollmentCreateDto, Enrollment>();
+            CreateMap<EnrollmentDto, Enrollment>();
+            CreateMap<Enrollment, EnrollmentDto>();
+        }
+    }
+}
