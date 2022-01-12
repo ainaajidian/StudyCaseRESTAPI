312616026627dbf4294b6fe96e0b1980276e2b85
ainaajidian <ainaajidian@gmail.com>
2022-01-12 10:28:02 +0700

    Add Validation

---
+++ EnrollmentService/ValidationAttributes/FirstAndLastNameMustBeDifferentAttribute.cs
@@ -0,0 +1,21 @@
+﻿using EnrollmentService.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentService.ValidationAttributes
+{
+    public class StudentFirstLastMustBeDifferentAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var student = (StudentCreateDto)validationContext.ObjectInstance;
+            if (student.FirstName == student.LastName)
+            {
+                return new ValidationResult("FirstName dan LastName tidak boleh sama",
+                    new[] { nameof(StudentCreateDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
