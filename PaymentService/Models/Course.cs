75e5520958bff39dd7599c2f7817b4c18685d0ea
ainaajidian <ainaajidian@gmail.com>
2022-01-12 14:09:49 +0700

    Scaffold Database

---
+++ PaymentService/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PaymentService.Models
+{
+    public partial class Course
+    {
+        public Course()
+        {
+            Enrollments = new HashSet<Enrollment>();
+        }
+
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int CourseCredits { get; set; }
+        public double CoursePrice { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
