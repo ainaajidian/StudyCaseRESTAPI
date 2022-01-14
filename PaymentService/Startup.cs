c1a1281cb8e70f7f310232efc8080cd67c1dc1c0
ainaajidian <ainaajidian@gmail.com>
2022-01-14 21:59:09 +0700

    Update Connection String

--- PaymentService/Startup.cs
+++ PaymentService/Startup.cs
@@ -19,18 +19,31 @@ namespace PaymentService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
 
+        private readonly IWebHostEnvironment _env;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("LocalConnection")));
+            if (_env.IsProduction())
+            {
+                Console.WriteLine("--> Using Sql Server Linux 2019 Db");
+                services.AddDbContext<AppDbContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("PaymentsConn")));
+            }
+            else
+            {
+                Console.WriteLine("--> Using Local Sql Server");
+                services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("LocalConnection")));
+            }
 
             services.AddScoped<IPayment, PaymentDAL>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
